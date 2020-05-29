4d86283b29908426538b34c0d61316f05f6269eb
Monika Messerer <monika.messerer@telekom.de>
2020-05-29 13:37:44 +0200

    initial commit

---
+++ src/main/java/de/telekom/mayo/bonus/MayoBonusApplication.java
@@ -0,0 +1,13 @@
+package de.telekom.mayo.bonus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayoBonusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayoBonusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/telekom/mayo/bonus/MayoBonusApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telekom.mayo.bonus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayoBonusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
